606792f3100561e7eb420305937ce9f1045e5bf5
Jack Gerulskis <jfgerulskis@wpi.edu>
2025-07-23 20:34:48 -0400

    fix: redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,5 @@ import netlify from '@astrojs/netlify/functions';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: netlify(),
-  redirects: {
-    '/mymeetings': '/myMeetings',
-    '/MyMeetings': '/myMeetings',
-    '/MYMEETINGS': '/myMeetings',
-    '/mymeetings/privacypolicy': '/myMeetings/privacyPolicy',
-    '/mymeetings/termsofservice': '/myMeetings/termsOfService'
-  }
+  adapter: netlify()
 });
